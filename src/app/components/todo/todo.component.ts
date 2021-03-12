b29b23096a10871658e51a1c3829b3d15b96abbb
Alish Safarli <57995570+AlishSafarli@users.noreply.github.com>
2021-03-12 18:54:52 +0400

    12 March

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from 'src/app/models/todo';
+import { ToDoResponseModel } from 'src/app/models/ToDoRespondeModel';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  apiUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todos: ToDo[] = [];
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getToDos();
+  }
+
+  getToDos() {
+    this.httpClient.get<ToDo[]>(this.apiUrl).subscribe((data) => {
+      this.todos = data;
+    });
+  }
+}
